5fa04d8f6ab9f2721ba611a4389b16a92b7c988d
Elijah <elijahtahadlangit7@gmail.com>
2026-09-22 12:36:52 +0800

    Create basic_info.cpp

---
+++ basic_info.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string student_name = "Elijah";
+    int Age = 18;
+    
+    std::cout << "Student's Name: " << student_name << std::endl;
+    std::cout << "Student's Age: " << Age << std::endl;
+    
+    return 0;
+}
